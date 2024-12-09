cb3f0cf9c4026bb596466e7aa4bccd8ff96984d3
Josh Hodges <sick_libra@hotmail.com>
2024-12-08 19:14:59 -0500

    complete day3 puzzle 1

--- AOC_2024/Day4/FindXmas1.py
+++ AOC_2024/Day4/FindXmas1.py
@@ -30,24 +30,24 @@ def accessfile(file):
 
 def countXMAS(lines):
     count=getStraights(lines)
-    # count+=getVerticals(lines)
+    count+=getVerticals(lines)
+    count+=getDiagonals(lines)
     print(count)
 
 
 def getStraights(lines):
     count=0    
     for line in lines:
-        print(count)
+        # print(count)
         i=0
         j=0
         while (i>=0):
-            i=line.find('XMAS',i, len(line)-3)
-            
+            i=line.find('XMAS',i, len(line))            
             if i>=0:
                 count+=1
                 i+=1
         while (j>=0):
-            j=line.find('SAMX', j, len(line)-3)
+            j=line.find('SAMX', j, len(line))
             if j>=0:
                 count+=1
                 j+=1
@@ -59,36 +59,58 @@ def getVerticals(lines):
     count=0
     for line in lines:
         lineindex=lines.index(line)
-        for i in range(len(line)-1):
+        for i in range(len(line)):
             if line[i]=="X":
-                if lineindex>3 and lineindex<len(lines)-4 and (i>3 and i<len(line)-4):
-                    count+=getUp(lines, lineindex, i)
-                    count+=getUpRight(lines, lineindex, i)
-                    count+=getUpLeft(lines, lineindex, i)
+                if lineindex<3:
                     count+=getDown(lines, lineindex, i)
-                    count+=getDownLeft(lines, lineindex, i)
-                    count+=getDownRight(lines, lineindex, i)
-                elif lineindex<3:
+                elif lineindex>=len(line)-3:
+                    count+= getUp(lines, lineindex, i)
+                else:
                     count+=getDown(lines, lineindex, i)
-                    if i>3 and i<len(line)-4:
-                        count+=getDownRight(lines, lineindex,i)
+                    count+=getUp(lines, lineindex, i)                
+    return count
+
+def getDiagonals(lines):
+    count=0
+    for line in lines:
+        lineindex=lines.index(line)
+        for i in range(len(line)):
+            if line[i]=="X":
+                #cover for top 3 lines of file
+                if lineindex<3:
+                    if i<3:
+                        count+=getDownRight(lines, lineindex, i)
+                    elif i>=len(line)-3:
                         count+=getDownLeft(lines, lineindex, i)
                     else:
-                        if i<3:
-                            count+=getDownRight(lines, lineindex,i)
-                        else:
-                            count+=getDownLeft(lines, lineindex, i)
-                elif lineindex > (len(lines)-4):
-                    count+=getUp(lines, lineindex, i)
-                    if i>3 and i<len(line)-4:
-                        count+=getUpRight(lines, lineindex,i)
+                        count+=getDownLeft(lines, lineindex, i)
+                        count+=getDownRight(lines, lineindex, i)
+                #cover for last 4 lines of file
+                elif lineindex>=len(lines)-3:
+                    if i<3:
+                        count+=getUpRight(lines, lineindex, i)
+                    elif i>=len(line)-3:
                         count+=getUpLeft(lines, lineindex, i)
                     else:
-                        if i<3:
-                            count+=getUpRight(lines, lineindex,i)
-                        else:
-                            count+=getUpLeft(lines, lineindex, i)
+                        count+=getUpRight(lines, lineindex, i)
+                        count+=getUpLeft(lines, lineindex, i)
+                #any middle line of the file
+                else:
+                    if i<3:
+                        count+=getUpRight(lines, lineindex, i)
+                        count+=getDownRight(lines, lineindex, i)
+                    elif i>=len(line)-3:
+                        count+=getUpLeft(lines, lineindex, i)
+                        count+=getDownLeft(lines, lineindex, i)
+                    else:
+                        count+=getUpLeft(lines, lineindex, i)
+                        count+=getUpRight(lines, lineindex, i)
+                        count+=getDownLeft(lines, lineindex, i)
+                        count+=getDownRight(lines, lineindex, i)
     return count
+
+
+
 
 def getDown(lines, lineindex, ind):
     if((lines[lineindex+1][ind]=="M") and (lines[lineindex+2][ind]=="A") and (lines[lineindex+3][ind]=="S")):
@@ -132,3 +154,32 @@ def main():
     countXMAS(lines)
 
 main()
+
+"""if (lineindex>2 and lineindex<len(lines)-3) and (i>2 and i<len(line)-3):
+                    count+=getUp(lines, lineindex, i)
+                    count+=getUpRight(lines, lineindex, i)
+                    count+=getUpLeft(lines, lineindex, i)
+                    count+=getDown(lines, lineindex, i)
+                    count+=getDownLeft(lines, lineindex, i)
+                    count+=getDownRight(lines, lineindex, i)
+                else:
+                    if lineindex<=2:
+                        count+=getDown(lines, lineindex, i)
+                        if i>2 and i<len(line)-3:
+                            count+=getDownRight(lines, lineindex,i)
+                            count+=getDownLeft(lines, lineindex, i)
+                        else:
+                            if i<2:
+                                count+=getDownRight(lines, lineindex,i)
+                            else:
+                                count+=getDownLeft(lines, lineindex, i)
+                    if lineindex > (len(lines)-3):
+                        count+=getUp(lines, lineindex, i)
+                        if i>2 and i<len(line)-3:
+                            count+=getUpRight(lines, lineindex,i)
+                            count+=getUpLeft(lines, lineindex, i)
+                        else:
+                            if i<2:
+                                count+=getUpRight(lines, lineindex,i)
+                            else:
+                                count+=getUpLeft(lines, lineindex, i)"""
