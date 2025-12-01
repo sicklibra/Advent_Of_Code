df23d7e57e59494407a58ff15faa6160a31fb4f0
Josh H <rhodges12@hotmail.com>
2025-12-01 18:53:22 -0500

    begin day 1 2025

---
+++ AOC_2025/Day1/SafeCode1.py
@@ -0,0 +1,12 @@
+'''Day 1 Advent of code 2025
+Author: Josh Hodges
+
+The text file in this is arranged with a letter followed by a number. These are in reference to a dial that reads
+from 0-99. The number is how many 'clicks' the dial is to be turned and the letter is what direction. 
+the final result is a count of how many times the dial lands on 0 '''
+
+'''read the file to array containing the letter and number '''
+def readFile(file):
+
+
+def main():
