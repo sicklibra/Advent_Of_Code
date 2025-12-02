d2d4c811b276220ac30c3e4bec28014c5ee50de3
Josh H <rhodges12@hotmail.com>
2025-12-01 22:00:55 -0500

    add comment and file read

--- AOC_2025/Day1/SafeCode1.py
+++ AOC_2025/Day1/SafeCode1.py
@@ -6,7 +6,19 @@ from 0-99. The number is how many 'clicks' the dial is to be turned and the lett
 the final result is a count of how many times the dial lands on 0 '''
 
 '''read the file to array containing the letter and number '''
+
 def readFile(file):
+    infile=open(file,'r')
+    line=infile.readline()
+    lnlst=[]
+    while line!='':
+        lnlst.append(line.trim())
+        line=infile.readline()
+    return lnlst
 
-
+''' The way to handle this one is to take the numbers and add or subtract in accordance with left and right
+left will be addition and right will be subtraction. Take the final number and take the abs value of that number and 
+if it is over 1000 mod 1000 then mod 100 and that will give me the 0-99 value that I am looking for
+then if it is 0 add it to the count.'''
 def main():
+    lns = readFile('day1_input.txt')
